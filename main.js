31ae337648643fceb0eb46872f6eb562c0b8760d
William Grant <willmgrant@gmail.com>
2017-07-30 22:12:24 +0200

    Added a basic UI

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ const app = electron.app
 
 app.on('ready', event=>{
     'use strict';
-    const win = new BrowserWindow({width:600, height:200});
+    const win = new BrowserWindow({width:600, height:400, titleBarStyle: 'hiddenInset'}); //Strictly macOS
     win.loadURL(`file://${__dirname}/html/index.html`);
 });
