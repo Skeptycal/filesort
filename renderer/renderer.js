95cc973489f38c32addf05490739827d258e7e50
William Grant <willmgrant@gmail.com>
2017-07-30 19:17:41 +0200

    Now supports torrents.

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,5 +1,6 @@
 const jetpack = require('fs-jetpack');
 const parseTorrentFile = require('parse-torrent-file');
+const fs = require('fs');
 
 //file extensions
 const bookExt = ["*.epub", "*.mobi"];
@@ -28,39 +29,55 @@ function sortFiles(path) {
 
         // _Books
         let bookFiles = jetMaster.find('.', {matching: bookExt, recursive: false});
-        moveFiles(bookFiles, `_Books`, jetMaster);
+        if (bookFiles.length > 0){
+            moveFiles(bookFiles, `_Books`, jetMaster);
+        }
 
         // _Documents
         let docFiles = jetMaster.find('.', {matching: docExt, recursive: false});
-        moveFiles(docFiles, `_Documents`, jetMaster);
-
+        if (docFiles.length > 0){
+            moveFiles(docFiles, `_Documents`, jetMaster);
+        }
         // _Images
         let imageFiles = jetMaster.find('.', {matching: imageExt, recursive: false});
-        moveFiles(imageFiles, `_Images`, jetMaster);
-
+        if (imageFiles.length > 0){
+            moveFiles(imageFiles, `_Images`, jetMaster);
+        }
         // _Music
         let musicFiles = jetMaster.find('.', {matching: musicExt, recursive: false});
-        moveFiles(musicFiles, `_Music`, jetMaster);
+        if (musicFiles.length > 0){
+            moveFiles(musicFiles, `_Music`, jetMaster);
+        }
 
         // _Programs
         let programFiles = jetMaster.find('.', {matching: programExt, recursive: false});
-        moveFiles(programFiles, `_Programs`, jetMaster);
+        if (programFiles.length > 0){
+            moveFiles(programFiles, `_Programs`, jetMaster);
+        }
 
         // _Scripts
         let scriptFiles = jetMaster.find('.', {matching: scriptExt, recursive: false});
-        moveFiles(scriptFiles, `_Scripts`, jetMaster);
+        if (scriptFiles.length > 0){
+            moveFiles(scriptFiles, `_Scripts`, jetMaster);
+        }
 
         // _Torrents
         let torrentFiles = jetMaster.find('.', {matching: torrentExt, recursive: false});
-        moveFiles(torrentFiles, `_Torrents`, jetMaster);
+        if (torrentFiles.length > 0){
+            moveFiles(torrentFiles, `_Torrents`, jetMaster);
+        }
 
         // _Videos
         let videoFiles = jetMaster.find('.', {matching: videoExt, recursive: false});
-        moveFiles(videoFiles, `_Videos`, jetMaster);
+        if (videoFiles.length > 0){
+            moveFiles(videoFiles, `_Videos`, jetMaster);
+        }
 
         // _Zipped
         let zippedFiles = jetMaster.find('.', {matching: zippedExt, recursive: false});
-        moveFiles(zippedFiles, `_Zipped`, jetMaster);
+        if (zippedFiles.length > 0){
+            moveFiles(zippedFiles, `_Zipped`, jetMaster);
+        }
 
         //find all remaining directories
 
@@ -70,11 +87,31 @@ function sortFiles(path) {
 }
 
 function moveFiles(files, typeFolder, jetPath) {
-    console.log(files);
+    // console.log(files);
     for(let file of files) {
-        console.log(`Moving from ${jetPath.cwd()}/${file} to ${jetPath.cwd()}/${typeFolder}/${file}`)
+        if(typeFolder === `_Torrents`) {
+            var torrent = fs.readFileSync(jetPath.cwd()+'/'+file);
+            var parsed;
+            try {
+              parsed = parseTorrentFile(torrent);
+            } catch (e) {
+              // the torrent file was corrupt
+              console.error(e);
+            }
+            //check existence
+            if(jetPath.exists(parsed.name)!=false) {
+                console.log(`Moving from ${jetPath.cwd()}/${parsed.name} to ${jetPath.cwd()}/${typeFolder}/${parsed.name}`);
+                jetPath.dir(`${typeFolder}`);
+                jetPath.move(`${jetPath.cwd()}/${parsed.name}`, `${jetPath.cwd()}/${typeFolder}/${parsed.name}`);
+            } else {
+                console.log("WARNING: Torrent files are missing.");
+            }
+        }
+
+        console.log(`Moving from ${jetPath.cwd()}/${file} to ${jetPath.cwd()}/${typeFolder}/${file}`);
         jetPath.dir(`${typeFolder}`);
         jetPath.move(`${jetPath.cwd()}/${file}`, `${jetPath.cwd()}/${typeFolder}/${file}`);
+
     }
-    // console.log(`Moved files to ${typeFolder}.`)
+    // console.log(`Moved files to ${typeFolder}.`);
 }
