0b5765dfb5ca60934c8785ebfdda79dbdf574503
William Grant <willmgrant@gmail.com>
2017-07-30 12:53:35 +0200

    Added ext arrays and can now find files based on type!

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,15 +1,35 @@
 const jetpack = require('fs-jetpack');
 
+//file extensions
+const bookExt = ["*.epub", "*.mobi"];
+const docExt = ["*.pdf", "*.txt", "*.doc", "*.docx", "*.dotx","*.ppt", "*.pptx", "*.md", "*.json", "*.ods", "*.log", "*.xls", "*.xlsx"];
+const imageExt = ["*.png", "*.jpg", "*.jpeg", "*.gif", "*.xcf", "*.stl", "*.blend"];
+const musicExt = ["*.mp3", "*.wav", "*.flac", "*.m4a", "*.ogg", "*.mid", "*.asd", "*.m3u", "*.pls", "*.alp", "*.asx", "*.bfxrsound"];
+const programsExt = ["*.dmg", "*.exe", "*.sh", "*.app", "*.pkg", "*.apk", "*.ipa"];
+const scriptExt = ["*.py", "*.java", "*.class", "*.sh"];
+const torrentExt = ["*.torrent"];
+const videoExt = ["*.mkv", "*.mp4", "*.mov", "*.mpeg"];
+const zippedExt = ["*.zip", "*.rar", "*.7z", "*.tar.gz", "*.tar", "*.gz", "*.unitypackage"];
+
 function sortFiles(path) {
-    console.log("sortFiles - start");
-    let files = jetpack.list(path);
+    const jetMaster = jetpack.cwd(path);
+    console.log(`Master folder: ${jetMaster.cwd()}`);
+
+    let files = jetMaster.list(path);
     if (files === undefined){
         console.log("Read error");
     } else{
-        for(let file of files) {
-            console.log(file);
+        // for(let file of files) {
+        //     console.log(file);
+        // }
+        // run through each type of file
+        let musicFiles = jetMaster.find('.', {matching: musicExt, recursive: false});
+        for(let file of musicFiles) {
+            console.log(`_Music: ${file}`);
         }
+        //find all remaining directories
+
     }
 
-    console.log("sortFiles - end");
+    console.log("sorted");
 }
